97b0024d9b689cda4686bf1661e55993d3172946
Rafał Święcicki <s31225@pjwstk.edu.pl>
2026-03-23 21:25:16 +0100

    feat: add bodies for users classes

--- Model/Employee.cs
+++ Model/Employee.cs
@@ -1,6 +1,8 @@
 ﻿namespace apbd_cw2_git_s31225.Model;
 
-public class Employee
+public class Employee(string id, string name, string surname, string title, string subject)
+    : User(id, name, surname)
 {
-    
+    public string Title { get; set; } = title;
+    public string Subject { get; set; } = subject;
 }
--- Model/Student.cs
+++ Model/Student.cs
@@ -1,6 +1,8 @@
 ﻿namespace apbd_cw2_git_s31225.Model;
 
-public class Student
+public class Student(string id, string name, string surname, string indexNumber, string faculty)
+    : User(id, name, surname)
 {
-    
+    public string IndexNumber { get; set; } = indexNumber;
+    public string Faculty { get; set; } = faculty;
 }
