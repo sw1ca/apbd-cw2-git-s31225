677752e1a6b033a240f67943c2ab6d62e13e86a4
Rafał Święcicki <s31225@pjwstk.edu.pl>
2026-03-23 20:32:21 +0100

    feat: add device class

---
+++ Model/Device.cs
@@ -0,0 +1,15 @@
+﻿namespace apbd_cw2_git_s31225.Model;
+
+public class Device
+{
+    public string Id;
+    public string Name;
+    public bool isAvailable;
+
+    public Device(string id, string name)
+    {
+        Id = id;
+        Name = name;
+        isAvailable = true;
+    }
+}
