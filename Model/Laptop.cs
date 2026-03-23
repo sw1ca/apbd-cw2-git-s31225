bb7c935f7ebee7f3aab39fb7adea776cce000b99
Rafał Święcicki <s31225@pjwstk.edu.pl>
2026-03-23 20:35:53 +0100

    feat: add laptop class as the example of device

---
+++ Model/Laptop.cs
@@ -0,0 +1,13 @@
+﻿namespace apbd_cw2_git_s31225.Model;
+
+public class Laptop : Device
+{
+    public int Ram;
+    public string Procesor;
+
+    public Laptop(string id, string name, int ram, string procesor) : base(id, name)
+    {
+        Ram = ram;
+        Procesor = procesor;
+    }
+}
