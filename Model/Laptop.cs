b7c3ac77d9e3bb23982f0a65a0b75d649e6020b6
Rafał Święcicki <s31225@pjwstk.edu.pl>
2026-03-23 21:16:14 +0100

    fix: formatting change

--- Model/Laptop.cs
+++ Model/Laptop.cs
@@ -1,6 +1,7 @@
 ﻿namespace apbd_cw2_git_s31225.Model;
 
-public class Laptop(string id, string name, bool isAvailable, int ram, string procesor) : Device(id, name, isAvailable)
+public class Laptop(string id, string name, bool isAvailable, int ram, string procesor)
+    : Device(id, name, isAvailable)
 {
     public int Ram { get; set; } = ram;
     public string Procesor { get; set; } = procesor;
--- Model/Projector.cs
+++ Model/Projector.cs
@@ -1,6 +1,7 @@
 ﻿namespace apbd_cw2_git_s31225.Model;
 
-public class Projector(string id, string name, bool isAvailable, string resolution, int brightness) : Device(id, name, isAvailable)
+public class Projector(string id, string name, bool isAvailable, string resolution, int brightness)
+    : Device(id, name, isAvailable)
 {
     public string Resolution { get; set; } = resolution;
     public int Brightness { get; set; } = brightness;
