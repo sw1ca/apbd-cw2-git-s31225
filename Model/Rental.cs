a5e71570e746593a450ee2826be14f7a0ad3bc19
Rafał Święcicki <s31225@pjwstk.edu.pl>
2026-03-23 21:35:07 +0100

    feat: add Rental class

--- Model/Rental.cs
+++ Model/Rental.cs
@@ -1,6 +1,13 @@
 ﻿namespace apbd_cw2_git_s31225.Model;
 
-public class Rental
+public class Rental(Device item, User borrower, DateTime rentalDate, int daysLimit)
 {
+    public Device Item { get; set; } = item;
+    public User Borrower { get; set; } = borrower;
     
+    public DateTime RentalDate { get; set; } = rentalDate;
+    public DateTime DueDate { get; set; } = rentalDate.AddDays(daysLimit);
+    public DateTime? ReturnDate { get; set; } = null; // null = not returned yet
+    
+    public decimal Fine { get; set; } = 0; // penalty 
 }
