72795fd3c794a8508dad120d81acb268b8078022
Rafał Święcicki <s31225@pjwstk.edu.pl>
2026-03-23 21:27:01 +0100

    feat: add body of class user

--- Model/User.cs
+++ Model/User.cs
@@ -1,6 +1,8 @@
 ﻿namespace apbd_cw2_git_s31225.Model;
 
-public class User
+public class User(string id, string name, string surname)
 {
-    
+    public string Id { get; set; } = id;
+    public string Name { get; set; } = name;
+    public string Surname { get; set; } = surname;
 }
